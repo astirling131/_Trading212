e6b8d2b1cd6aaede30e6f472a8f95b9279cf290d
astirling131 <andrewstirling131@gmail.com>
2026-01-23 17:23:12 +0000

    Feat: Added YFinance class to get historic market data

--- main.py
+++ main.py
@@ -8,8 +8,6 @@
 ## Usage: python main.py                                                                                              ##
 ########################################################################################################################
 
-
-
 #################
 # CONFIGURATION #
 #################
@@ -19,17 +17,21 @@ import requests
 import base64
 import time
 import pandas as pd
+import os
+import yfinance as yf
 from datetime import datetime, timedelta, timezone
 from typing import Optional, Dict, Any, Tuple
+from twelvedata import TDClient
 
+# function to get the api key and secret from the .env file
 def _get_api_keys(provider: str) -> Tuple[str, str]:
-    # get the api key and secret from the .env file
+    # initialize variables
     api_key_id, api_secret = None, None
     # run inside a try block for error handling
     try:
         # Check the requested provider
         if provider == "Trading212":
-            # load data from the .env file into 'file' variable
+            # load data from the .env file into variables
             with open(".env", "r") as file:
                 for line in file:
                     parts = line.strip().split("=")
@@ -64,8 +66,8 @@ class Trading212Client:
     # define the specific api endpoints
     ENDPOINT_CASH = "/equity/account/cash"
     ENDPOINT_HISTORY = "/equity/history/exports"
-    ENDPOINT_TICKER = "/equity/ / "
 
+    # class constructor
     def __init__(self, is_demo: bool = False):
         # define the URL we want to 'call'.
         if is_demo:
@@ -74,90 +76,116 @@ class Trading212Client:
            self.url = "https://live.trading212.com/api/v0"
         self.headers = self._set_auth_header()
 
+    ##~~~~~~~~~~~~~~~~~~
+    ## HELPER FUNCTIONS
+    ##~~~~~~~~~~~~~~~~~~
+
+    # function to set the authorization header for requests
     def _set_auth_header(self) -> Dict[str, str]:
-        # add the api_key to a variable for use in the function
+        # get the T212 api keys from the .env file
         key_id, secret_key = _get_api_keys("Trading212")
         # exit the function if we didn't get a key
         if not key_id or not secret_key:
             print("\nError: Credentials missing in .env file.")
             return
-        # create the authorization header value
+        # create the authorization header value and encode to UTF-8 (bytes)
         credentials_string = f"{key_id}:{secret_key}"
-        # encode the string credentials to UTF-8 (bytes)
         raw_bytes = credentials_string.encode("utf-8")
         # encode the bytes to Base64 then back to useable string
         base64_bytes = base64.b64encode(raw_bytes)
         base64_string = base64_bytes.decode("utf-8")
         # format the final authorization header value
         auth_header_value = f"Basic {base64_string}"
-        # create the headers dictionary for the GET request
         headers = {
             "Authorization": auth_header_value
         }
         return headers
 
+    # function to make HTTP requests
     def _make_request(self, method: str, endpoint: str, payload: Optional[dict] = None) -> Any:
+        # construct the full URL
         url = self.url + endpoint
         print(f"\nSending {method} request to: {endpoint}")
+        # check if headers are set
         if not self.headers:
             print("Headers not set, check API keys")
             return None
+        # run inside a try catch block for error handling
         try:
+            # perform the HTTP request based on the method
             if method == "GET":
                 response = requests.get(url, headers=self.headers)
             elif method == "POST":
                 response = requests.post(url, headers=self.headers, json=payload)
             else:
                 raise ValueError("Unsupported HTTP request")
-
+            # check the response status code
             if response.status_code == 200:
+                # on successful request return the JSON data
                 return response.json()
             else:
+                # on failed request print the status code and reason
                 print(f"Request failed. Status: {response.status_code}")
                 print(f"Reason: {response.text}")
                 return None
-
+        # catch any exceptions that occur during the request
         except Exception as e:
             print(f"An error occurred during request: {e}")
             return None
 
+    # function to poll for report completion
     def _poll_for_completion(self, report_id: int) -> Optional[str]:
+        # define the endpoint
         endpoint = self.ENDPOINT_HISTORY
         # set loop variable
         attempts: int = 10
         count: int = 1
         # run inside a try catch block for error handling
         try:
+            # loop until max attempts
             while count < attempts:
                 print(f"Checking status, attempt {count} of {attempts}...")
+                # perform GET request on endpoint
                 exports = self._make_request("GET", endpoint)
+                # wait before next attempt
                 time.sleep(5)
+                # check if the request returned any exports
                 if exports:
                     target_report = None
+                    # find the report with the matching report ID
                     for report in exports:
                         if report.get("reportId") == report_id:
                             target_report = report
+                            #  exit the loop early if theres no report to wait for
                             break
+                    # make sure it found the target report
                     if target_report:
                         status = target_report.get("status")
+                        # check if the report is finished
                         if status == "Finished":
                             print(f"Status is {status}")
+                            # return the download link
                             return target_report.get("downloadLink")
+                        # wait 5 seconds before next loop iteration if report is not ready to download
                         time.sleep(5)
+        # catch any exceptions that occur during polling
         except Exception as e:
             print(f"An error occurred while polling for report completion: \n{e}")
             return None
+        # if we reach here it timed out
         print("\nTimed out waiting on report")
         return None
-
-    ##################
-    # MAIN EXECUTION #
-    ##################
+    
+    ##~~~~~~~~~~~~~~~~~~
+    ## CLASS FUNCTIONS
+    ##~~~~~~~~~~~~~~~~~~
 
     # function to fetch account cash data
     def fetch_account_cash(self) -> None:
         endpoint = self.ENDPOINT_CASH
         data = self._make_request("GET", endpoint)
+        # wait for response from requeest
+        time.sleep(5)
         if data:
             print(f"\n---CASH DATA---")
             print(data)
@@ -200,16 +228,56 @@ class Trading212Client:
             else:
                 print(".csv report was empty.")
 
-##    def download_ticker_data(self) -> None:
-##        endpoint = self.ENDPOINT_TICKER
-        
-#####################
-# EXECUTION CONTROL #
-#####################
+##################
+# YFinance CLASS #
+##################
 
-# This line ensures the script runs only when executed directly (if the .py script is imported the __name__ variable will be just 'main' and not '__main__')
+class YFinanceClient:
+
+    # class constructor
+    def __init__(self):
+        pass
+
+    # function to get historic market data  
+    def get_historic_data(self, symbol: str, interval: str = "15m", period: str = "1mo") -> None:
+        print(f"\n--- Fetching {interval} data for {symbol} ---")
+        if symbol in ["CSP1", "VHYL", "IDVY"] and not symbol.endswith(".L"):
+            symbol = f"{symbol}.L"
+        try:
+            df = yf.download(
+                tickers=symbol, 
+                interval=interval, 
+                period=period,
+                progress=False
+            )
+            if not df.empty:
+                # Yahoo returns complex MultiIndex columns sometimes
+                df.reset_index(inplace=True)
+                # Save to Disk
+                folder = "market_data"
+                os.makedirs(folder, exist_ok=True)
+                filename = f"{folder}/{symbol}_{interval}.csv"
+                df.to_csv(filename, index=False)
+                print(f"Success! Saved {len(df)} rows to {filename}")
+                print(df.head(3))
+            else:
+                print(f"Error: No data found for {symbol}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+     
+##################
+# MAIN EXECUTION #
+##################
+
+# Execution control
 if __name__ == "__main__":
-    client = Trading212Client(is_demo=False)
-    client.fetch_account_cash()
-    client.download_historic_data()
-##    client.download_ticker_data()
+
+    # 1. Trading212 Client Usage
+    client001 = Trading212Client(is_demo=False)
+    client001.fetch_account_cash()
+    client001.download_historic_data()
+
+    # 2. YFinanceClient Client Usage
+    client002 = YFinanceClient()
+    client002.get_historic_data(symbol="CSP1")
+    
